d292166218394561084b5b2e60dc90204aefc2d9
草鞋没号 <308487730@qq.com>
2019-04-22 09:09:06 +0800

    19-04-22 RecyclerView layout.

--- app/src/main/java/com/atom/imoocmusic/activitys/MainActivity.java
+++ app/src/main/java/com/atom/imoocmusic/activitys/MainActivity.java
@@ -1,10 +1,16 @@
 package com.atom.imoocmusic.activitys;
 
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 import com.atom.imoocmusic.R;
+import com.atom.imoocmusic.adapters.MusicGridAdapter;
 
 public class MainActivity extends BaseActivity {
+
+    private RecyclerView mRvGrid;
+    private MusicGridAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +22,10 @@ public class MainActivity extends BaseActivity {
 
     private void initView() {
         initNavBar(false, "Fire music", true);
+
+        mRvGrid = fd(R.id.rv_grid);
+        mRvGrid.setLayoutManager(new GridLayoutManager(MainActivity.this, 3));
+        mAdapter = new MusicGridAdapter(MainActivity.this);
+        mRvGrid.setAdapter(mAdapter);
     }
 }
--- app/src/main/java/com/atom/imoocmusic/adapters/MusicGridAdapter.java
+++ app/src/main/java/com/atom/imoocmusic/adapters/MusicGridAdapter.java
@@ -1,4 +1,41 @@
 package com.atom.imoocmusic.adapters;
 
-public class MusicGridAdapter {
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.atom.imoocmusic.R;
+
+public class MusicGridAdapter extends RecyclerView.Adapter<MusicGridAdapter.ViewHolder> {
+    private Context mContext;
+
+    public MusicGridAdapter (Context context) {
+        mContext = context;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        return new ViewHolder(LayoutInflater.from(mContext).inflate(R.layout.item_grid_music, viewGroup, false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 6;
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
 }
