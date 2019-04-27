a2a8c33dd2f7b5a249c6f953128af254755f2c02
草鞋没号 <308487730@qq.com>
2019-04-27 08:36:00 +0800

    19-04-27 高斯模糊效果

--- app/src/main/java/com/atom/imoocmusic/activitys/PlayMusicActivity.java
+++ app/src/main/java/com/atom/imoocmusic/activitys/PlayMusicActivity.java
@@ -3,10 +3,18 @@ package com.atom.imoocmusic.activitys;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.ImageView;
 
 import com.atom.imoocmusic.R;
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+
+import jp.wasabeef.glide.transformations.BlurTransformation;
 
 public class PlayMusicActivity extends BaseActivity {
+
+    private ImageView mIvBg;
+    private String imgUrl = "https://y.gtimg.cn/music/photo_new/T002R300x300M000003w7iL42J7TjH.jpg?max_age=2592000";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +23,16 @@ public class PlayMusicActivity extends BaseActivity {
 
         // 隐藏 statusBar
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        initView();
+        Glide.with(this)
+                .load(imgUrl)
+                .apply(RequestOptions.bitmapTransform(new BlurTransformation(25, 10)))
+                .into(mIvBg);
+    }
+
+    private void initView() {
+        mIvBg = fd(R.id.iv_bg);
     }
 
     public void onBackClick(View view) {
